2473d0f8178bd66adbd7e41d71e3e16902c0477e
Andre Viljoen <69426035+aviljoenn@users.noreply.github.com>
2025-08-03 06:53:47 +0200

    Create BraaSMAN.py

---
+++ BraaSMAN.py
@@ -0,0 +1,9 @@
+#!python.exe
+# -*- coding: utf-8 -*-
+# Author: Andre Viljoen
+
+# Last Update: 2025-07-27 16:15
+
+gSCRIPT = "BraasMAN Application"    #Application Name displayed in the header of the Application GUI
+gVER = "V0.1a"                      #Version is displayed together with the Application Name of the Application GUI
+#Note to developer: Continue developing the application from here...
